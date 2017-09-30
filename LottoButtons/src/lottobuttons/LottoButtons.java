fbe368c0ba7d9703d5328c88036c12152c3336f6
KevinReidIRL <kif20111@gmail.com>
2017-09-30 16:36:54 +0100

    KevinReidIRL 300917

---
+++ LottoButtons/src/lottobuttons/LottoButtons.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lottobuttons;
+
+/**
+ *
+ * @author Kevin
+ */
+public class LottoButtons {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LottoButtonsGUI gui = new LottoButtonsGUI();
+        gui.setVisible(true);
+    }
+    
+}
